93e28a7bcff1a05db4216ffdfd58daa0f4423db8
sadjin25 <qkrdnwls0101@naver.com>
2021-12-08 10:29:14 +0900

    #6 resolve a bit of err of highlight func

--- public/js/script.js
+++ public/js/script.js
@@ -38,32 +38,33 @@ $(function(){
   convertDateTime();
 });
 
-$(function() {
-  let search = window.location.search;
-  let params = {};
+$(function(){
+  var search = window.location.search; // 1
+  var params = {};
 
-  if(search) {
-    $.each(search.slice(1).split('&'), function(index,param) {
-      let index = param.indexOf('=');
-      if(index>0) {
-        let key = param.slice(0,index);
-        let value = param.slice(index+1);
+  if(search){ // 2
+    $.each(search.slice(1).split('&'),function(index,param){
+      var index = param.indexOf('=');
+      if(index>0){
+        var key = param.slice(0,index);
+        var value = param.slice(index+1);
 
         if(!params[key]) params[key] = value;
       }
     });
   }
 
-  if(params.searchText && params.searchText.length>=3) {
-    $('[data-search-highlight]').each(function(index, element) {
-      let $element = $(element);
-      let searchHighlight = $element.data('search-highlight');
-      let index = params.searchType.indexOf(searchHighlight);
-      if(index>=0) {
-        let decodedSearchText = params.searchText.replace(/\+/g, ' ');
-        decodedSearchText = decodedURI(decodedSearchText);
+  if(params.searchText && params.searchText.length>=3){ // 3
+    $('[data-search-highlight]').each(function(index,element){
+      var $element = $(element);
+      var searchHighlight = $element.data('search-highlight');
+      var index = params.searchType.indexOf(searchHighlight);
 
-        let regex = new RegExp(`(${decodedSearchText})`, 'ig');
+      if(index>=0){
+        var decodedSearchText = params.searchText.replace(/\+/g, ' '); //  3-1
+        decodedSearchText = decodeURI(decodedSearchText);
+        
+        var regex = new RegExp(`(${decodedSearchText})`,'ig'); // 3-2
         $element.html($element.html().replace(regex,'<span class="highlighted">$1</span>'));
       }
     });
